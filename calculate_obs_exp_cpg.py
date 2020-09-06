5680fd777a0f96696c7027f5d0439c7f30ec5b9d
Olga Tsiouri <olgatsiouri@outlook.com>
2020-09-06 21:58:00 +0300

    Update calculate_obs_exp_cpg.py

--- calculate_obs_exp_cpg.py
+++ calculate_obs_exp_cpg.py
@@ -7,7 +7,7 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-in", "--input_file", required=True, help="input fasta file")
 ap.add_argument("-gc", "--gc_content", required=True, help="min GC content")
 ap.add_argument("-ratio", "--gc_ratio", required=True, help="min ratio of the Obs/Exp value, type = float")
-ap.add_argument("-win", "--window_size", required=True, help="window size for CpG identification")
+ap.add_argument("-win", "--window_size", required=True, help="window size for CpG identification, type = float")
 ap.add_argument("-out", "--output_file", required=True, help="output txt file")
 args = vars(ap.parse_args())
 # calculate obs value
@@ -18,13 +18,13 @@ def exp(seq):
   return round(seq.count('C') * seq.count('G') / int(args['window_size']))
 # calculate gc content
 def gc_content(seq):
-  return round((seq.count('C') + seq.count('G')) / (seq.count('C') + seq.count('G') + seq.count('A') + seq.count('T')) * 100)
+  return round((seq.count('C') + seq.count('G')) / (seq.count('C') + seq.count('G') + seq.count('A') + seq.count('T')) * 100, 2)
 # main
 gcobs = []
 gcexp = []
 headers = [] # setup empty lists
 for record in SeqIO.parse(args['input_file'], "fasta"):
-    if gc_content(record.seq) > int(args['gc_content']):
+    if gc_content(record.seq) > float(args['gc_content']):
        gcobs.append(obs(record.seq))
        gcexp.append(exp(record.seq))
        headers.append(record.id)
