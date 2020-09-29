513201e384ad68127f0ee1f812ba13376a9694fe
Olga Tsiouri <olgatsiouri@outlook.com>
2020-09-29 21:50:51 +0300

    Update calculate_obs_exp_cpg.py

--- calculate_obs_exp_cpg.py
+++ calculate_obs_exp_cpg.py
@@ -42,7 +42,7 @@ df = df[df['exp'] > 0]
 df = df[df['obs/exp'] > float(args['gc_ratio'])]
 df = df.sort_values(by=['obs/exp'], ascending=False)
 df1 = pd.DataFrame()
-df1 = df.start.str.split("_", expand = True)
+df1 = df['start'].astype(str).str.split("_", expand = True)
 start = df1.iloc[:, 1]
 end = start.astype(int) + int(args['window_size'])
 df2 = pd.DataFrame()
