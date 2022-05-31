81a95b76d39877d1991121b106215b3b2e73bb5b
Olga Tsiouri <olgatsiouri@outlook.com>
2022-06-01 01:21:29 +0300

    added strand and %GC content columns fix scripts so that start location always < end location

--- README.md
+++ README.md
@@ -4,32 +4,33 @@ CpG island identification pipeline inspired by the  Gardiner-Garden and Frommer 
 
 ![](img/arguments.png) 
 
-Windows grafical user interface standalone programs is avaliable here
+Windows grafical user interface standalone programs is avaliable here:
 
 ![](img/program_gui.png)
 
 # **Binaries**
-The binary files for the windows and linux command line programs can be found: 
+The binary files for the linux command line program can be found here: 
 
 # **Depedences**
 
 1. python3.8 or later
 
-2. argparse: `pip3 install argparse`(for the command line)  
+2. argparse: `pip3 install argparse`(for the command line script)  
 
 3. biopython: `pip3 install biopython`
 
 4. pandas: `pip3 install pandas`  
 
-5. Gooey: (for the `gui.py` scripts) `pip install Gooey`
+5. Gooey: (for the `gui.py` script) `pip install Gooey`
 
 
 # **Example code**
 
- With the following script CpG islands are identified by selecting the %GC content, Obs/Exp ratio  window and step size. In this example sequence the existence of CpG islands is checked on 200 bp windows with 1 bp interval(the window moves 1bp each time):
-`cpg_island_identificator.py -in test.fasta -out cpg_table.txt -win 200 -step 1 -gc 55.00 -ratio 1.0`
-This script can be run also as GUI by running: `python3 cpg_island_identificator_gui.py` and a window will pop up
+ With the following script CpG islands are identified by selecting the %GC content, Obs/Exp ratio  window and step size. In this example sequence the existence of CpG islands is checked on 200 bp windows with 50 bp interval(the window moves 50bp each time):  
+`python3 cpg_island_identificator.py -in test.fasta -out cpg_table.txt -win 200 -step 50 -gc 55.00`
+This script can be run also as GUI by running:   
+`python3 cpg_island_identificator_gui.py` and a window will pop up
 
 The output file will look like: 
 
-![](img/example.png)
+![](img/example_output.png)
--- src/gui/cpg_island_identificator_gui.py
+++ src/gui/cpg_island_identificator_gui.py
@@ -9,56 +9,63 @@ import pandas as pd
 def main():
     ap = GooeyParser(description="identify CpG islands on one or many sequences based on the Gardiner-Garden and Frommer (1987) method")
     ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input single or multi-fasta file")
-    ap.add_argument("-gc", "--gc",  required=False, default=50, help="min GC content(support for S and W nucleotides)")
-    ap.add_argument("-ratio", "--ratio", required=False, default=0.6, help="min ratio of the Obs/Exp value, type = float")
-    ap.add_argument("-step", "--step", required=True, help="step size for CpG identification, type = integer")
-    ap.add_argument("-win", "--window", required=True, help="window size for CpG identification, type = integer")
+    ap.add_argument("-gc", "--gc",  required=False, type=int, default=50, help="min GC content(support for S and W nucleotides), integer")
+    ap.add_argument("-ratio", "--ratio", required=False, type=float, default=0.6, help="min ratio of the Obs/Exp value, float")
+    ap.add_argument("-step", "--step", required=True, type=int, help="step size for CpG identification, integer")
+    ap.add_argument("-win", "--window", required=True, type=int, help="window size for CpG identification, integer")
     ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output txt file")
     args = vars(ap.parse_args())
     # calculate obs value
     def obs(seq):
         return seq.count('CG')
     # calculate Exp value
-    def exp(seq):
-        return round(seq.count('C') * seq.count('G') / int(args['window']), 2)
+    def ratio(seq):
+        obs = seq.count('CG')
+        exp = seq.count('C') * seq.count('G') / int(args['window'])
+        return round(obs/exp, 2)
     # calculate gc content
     def gc(seq):
         gc = sum(seq.count(x) for x in ["G", "C", "S"])
         return round(gc * 100 / sum(seq.count(x) for x in ["A", "T", "G", "C", "S", "W"]), 2)
     # main
     gcobs = []
-    gcexp = []
+    gccont = []
     start = []
     end = []
-    headers = [] # setup empty lists
+    strand = []
+    headers = []
+    gcratio = [] # setup empty lists
     # import multi-fasta
     for record in SeqIO.parse(args['input'], "fasta"):
         rever = reverse_complement(record.seq)
     # forward sequence
-        for i in range(0, len(record.seq) - int(args['window']) + 1, int(args['step'])):
-            if gc(record.seq[i:i + int(args['window'])]) > float(args['gc']):
-                gcobs.append(obs(record.seq[i:i + int(args['window'])]))
-                gcexp.append(exp(record.seq[i:i + int(args['window'])]))
+        for i in range(0, len(record.seq) - args['window'] + 1, args['step']):
+            if gc(record.seq[i:i + args['window']]) >= args['gc'] and ratio(record.seq[i:i + args['window']]) >= args['ratio']:
+                gcobs.append(obs(record.seq[i:i + args['window']]))
+                gccont.append(gc(record.seq[i:i + args['window']]))
+                gcratio.append(ratio(record.seq[i:i + args['window']]))
                 start.append(i + 1) # fix python index
-                end.append(i + int(args['window'])) # retrieve the end position of each putative CpG island
+                end.append(args['window']) # retrieve the end position of each putative CpG island
                 headers.append(record.id)
+                strand.append('+')
     # reverse complement
-            if gc(rever[i:i + int(args['window'])]) > float(args['gc']):
-                gcobs.append(obs(rever[i:i + int(args['window'])]))
-                gcexp.append(exp(rever[i:i + int(args['window'])]))
-                start.append(i -len(record.seq))
-                end.append(i -len(record.seq) + int(args['window'])) # retrieve the end position of each putative CpG island
+            if gc(rever[i:i + args['window']]) >= args['gc'] and ratio(rever[i:i + args['window']]) >= args['ratio']:
+                gcobs.append(obs(rever[i:i + args['window']]))
+                gccont.append(gc(rever[i:i + args['window']]))
+                gcratio.append(ratio(rever[i:i + args['window']]))
+                start.append(-1*(i + args['window']  - len(record.seq)))
+                end.append(-1*(i - len(record.seq))) # retrieve the end position of each putative CpG island
                 headers.append(record.id)
+                strand.append('-')
     # create data frame
     df = pd.DataFrame()
     df['id'] = headers
     df['start'] = start
     df['end'] = end
+    df['strand'] = strand
+    df[''.join(['%','GC'])] = gccont
     df['obs'] = gcobs
-    df['exp'] = gcexp
-    df['obs/exp'] = round(df['obs']/df['exp'], 2) # calculate obs/exp ratio and filter by a user spesified threshold
-    df = df[df['exp'] > 0]
-    df = df[df['obs/exp'] > float(args['ratio'])]
+    df['obs/exp'] = gcratio
     df = df.sort_values(by=['obs/exp'], ascending=False) # sort by biggest obs/exp ratio
     # export
     with open(args['output'], 'a') as f:
